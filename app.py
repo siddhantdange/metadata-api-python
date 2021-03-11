e21c07fcad13ea7b035cc5f3144a04e49599bc72
Sid Dange <siddange@Sids-MacBook-Pro.local>
2021-03-10 23:09:46 -0800

    cleaned up requirements.txt + updated app to read from json file

--- app.py
+++ app.py
@@ -1,71 +1,36 @@
-from flask import Flask
-from flask import jsonify
-from google.cloud import storage
-from google.oauth2 import service_account
-from PIL import Image
+import json
 import os
 import mimetypes
 
-GOOGLE_STORAGE_PROJECT = os.environ['GOOGLE_STORAGE_PROJECT']
-GOOGLE_STORAGE_BUCKET = os.environ['GOOGLE_STORAGE_BUCKET']
+from flask import Flask
+from flask import jsonify
+
 
 app = Flask(__name__)
 
-FIRST_NAMES = ['Herbie', 'Sprinkles', 'Boris', 'Dave', 'Randy', 'Captain']
-LAST_NAMES = ['Starbelly', 'Fisherton', 'McCoy']
+with open('data/creatures.json') as f:
+    creatures = json.loads(f.read())
 
-BASES = ['jellyfish', 'starfish', 'crab', 'narwhal', 'tealfish', 'goldfish']
-EYES = ['big', 'joy', 'wink', 'sleepy', 'content']
-MOUTH = ['happy', 'surprised', 'pleased', 'cute']
+creatures_by_idx = {}
+for idx in range(len(creatures)):
+    creatures_by_idx[idx] = creatures[idx]
 
-
-INT_ATTRIBUTES = [5, 2, 3, 4, 8]
-FLOAT_ATTRIBUTES = [1.4, 2.3, 11.7, 90.2, 1.2]
-STR_ATTRIBUTES = [
-    'happy',
-    'sad',
-    'sleepy',
-    'boring'
-]
-BOOST_ATTRIBUTES = [10, 40, 30]
-PERCENT_BOOST_ATTRIBUTES = [5, 10, 15]
-NUMBER_ATTRIBUTES = [1, 2, 1, 1]
+def trait_by_type(creature, trait_type):
+    for attr in creature['attributes']:
+        if attr['trait_type'] == trait_type:
+            return attr['value']
+    return ''
 
 
 @app.route('/api/creature/<token_id>')
 def creature(token_id):
     token_id = int(token_id)
-    num_first_names = len(FIRST_NAMES)
-    num_last_names = len(LAST_NAMES)
-    creature_name = "%s %s" % (FIRST_NAMES[token_id % num_first_names], LAST_NAMES[token_id % num_last_names])
 
-    base = BASES[token_id % len(BASES)]
-    eyes = EYES[token_id % len(EYES)]
-    mouth = MOUTH[token_id % len(MOUTH)]
-    image_url = _compose_image(['images/bases/base-%s.png' % base,
-                                'images/eyes/eyes-%s.png' % eyes,
-                                'images/mouths/mouth-%s.png' % mouth],
-                               token_id)
+    creature = creatures_by_idx[token_id]
+    if not creature:
+        return None
 
-    attributes = []
-    _add_attribute(attributes, 'base', BASES, token_id)
-    _add_attribute(attributes, 'eyes', EYES, token_id)
-    _add_attribute(attributes, 'mouth', MOUTH, token_id)
-    _add_attribute(attributes, 'level', INT_ATTRIBUTES, token_id)
-    _add_attribute(attributes, 'stamina', FLOAT_ATTRIBUTES, token_id)
-    _add_attribute(attributes, 'personality', STR_ATTRIBUTES, token_id)
-    _add_attribute(attributes, 'aqua_power', BOOST_ATTRIBUTES, token_id, display_type="boost_number")
-    _add_attribute(attributes, 'stamina_increase', PERCENT_BOOST_ATTRIBUTES, token_id, display_type="boost_percentage")
-    _add_attribute(attributes, 'generation', NUMBER_ATTRIBUTES, token_id, display_type="number")
-
-
-    return jsonify({
-        'name': creature_name,
-        'description': "Friendly OpenSea Creature that enjoys long swims in the ocean.",
-        'image': image_url,
-        'external_url': 'https://openseacreatures.io/%s' % token_id,
-        'attributes': attributes
-    })
+    return jsonify(creature)
 
 
 @app.route('/api/box/<token_id>')
@@ -122,37 +87,12 @@ def factory(token_id):
 def _add_attribute(existing, attribute_name, options, token_id, display_type=None):
     trait = {
         'trait_type': attribute_name,
-        'value': options[token_id % len(options)]
+            'value': options[token_id % len(options
+        )]
     }
     if display_type:
         trait['display_type'] = display_type
     existing.append(trait)
-
-
-def _compose_image(image_files, token_id, path="creature"):
-    composite = None
-    for image_file in image_files:
-        foreground = Image.open(image_file).convert("RGBA")
-
-        if composite:
-            composite = Image.alpha_composite(composite, foreground)
-        else:
-            composite = foreground
-
-    output_path = "images/output/%s.png" % token_id
-    composite.save(output_path)
-
-    blob = _get_bucket().blob(f"{path}/{token_id}.png")
-    blob.upload_from_filename(filename=output_path)
-    return blob.public_url
-
-
-def _get_bucket():
-    credentials = service_account.Credentials.from_service_account_file('credentials/google-storage-credentials.json')
-    if credentials.requires_scopes:
-        credentials = credentials.with_scopes(['https://www.googleapis.com/auth/devstorage.read_write'])
-    client = storage.Client(project=GOOGLE_STORAGE_PROJECT, credentials=credentials)
-    return client.get_bucket(GOOGLE_STORAGE_BUCKET)
 
 
 if __name__ == '__main__':
